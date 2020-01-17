4745769ed3b626eac013f4bc1612a59b0de90a70
Andre Patacas <guerreiroapatacas@gmail.com>
2020-01-17 18:16:37 +0000

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,7 @@
+var obj = {
+    a: 0,
+    b: 0,
+    c: "asd",
+    d: function (param) { console.log("yhey " + param); }
+};
+obj.d(obj.c);
---
+++ backend/src/app.ts
@@ -0,0 +1,7 @@
+const obj = {
+  a : 0,
+  b: 0,
+  c : "asd",
+  d :(param : String) : void =>{console.log(`yhey ${param}`)}
+}
+obj.d(obj.c)
